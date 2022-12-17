1f089e74a4586665cf8e811877ee70daf7a631b4
Van-Ivan <pibo0121016@sibupk.su>
2022-12-17 12:13:16 +0700

    ор

--- Lab6/Lab6/Program.cs
+++ Lab6/Lab6/Program.cs
@@ -14,9 +14,9 @@ namespace Lab6
             var i1 = new IntegratorQuad(eq1);
             var i2 = new IntegratorTrap(eq1);
             var i3 = new IntegratorSimpson(eq1);
-            Console.WriteLine(i1.Integrate(-10, 10));
-            Console.WriteLine(i2.Integrate(-10, 10));
-            Console.WriteLine(i3.Integrate(-10, 10));
+            Console.WriteLine(i1.Integrate(-100, 100));
+            Console.WriteLine(i2.Integrate(-100, 100));
+            Console.WriteLine(i3.Integrate(-100, 100));
             Console.WriteLine(i1.TitleMethod);
             Console.WriteLine(i2.TitleMethod);
             Console.WriteLine(i3.TitleMethod);
